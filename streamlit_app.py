7cb07fa165585ce92c0014a533c46a994b06f296
SHIVANI GUPTA <121291662+Shivani-gupta3199@users.noreply.github.com>
2022-12-24 11:57:40 +0530

    Update streamlit_app.py
    used panda lib and created dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
-
-import streamlit
-streamlit.title('My parents new healthy diner')
-streamlit.header('🥑🍞 Breakfast Menu 🥣 🥗  ')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import panda
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
